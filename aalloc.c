df90d96ad5cc3efad272cba807e0964f333be070
Chris Williams <1463243+chrsw@users.noreply.github.com>
2024-11-28 15:27:14 -0500

    Move argument allocation to aalloc() function
    Include option main() with -DMAIN.

--- aalloc.c
+++ aalloc.c
@@ -5,12 +5,32 @@
 #include <string.h>
 #include "aalloc.h"
 
+// aal_gp[0] is the first argument
+// aal_gp[1] is the second argument
+// aal_gp[n] is the nth argument 
 char **aal_gp;
 
 
+#ifdef MAIN
 // create an array of pointers dynamically allocated 
 // number of arguments is known at run time
 int main(int argc, char *argv[]) {
+
+    int n = argc;
+
+    if (aalloc(argc, argv) != 0) exit(1);
+
+    for (--n; n >= 0; n--)
+        printf("%s\n", aal_gp[n]); 
+
+    free(aal_gp);
+    return 0;
+}
+#endif // MAIN
+
+// allocate an array of pointers, one pointer for each
+// command line argument
+int aalloc(int argc, char *argv[]) {
 
     int i;
     int n = argc;
@@ -23,9 +43,5 @@ int main(int argc, char *argv[]) {
         strcpy(aal_gp[argc-1], argv[argc-1]);
     } while (argc-- > 1);
 
-    for (--n; n >= 0; n--)
-        printf("%s\n", aal_gp[n]); 
-
-    free(aal_gp);
     return 0;
 }
---
+++ aalloc.h
@@ -0,0 +1,7 @@
+#ifndef AALLOC_H
+#define AALLOC_H
+
+int aalloc(int argc, char *argv[]);
+extern char **aal_gp;
+
+#endif // AALLOC_H
