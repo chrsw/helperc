b03aa740a1927a9aabc826f2235ee237d877c09b
Chris Williams <1463243+chrsw@users.noreply.github.com>
2024-12-22 09:18:49 -0500

    randomargs: minor cleanup
    Remove unnecessary statements
    Add better comments
    Formatting fixes

--- randomargs.c
+++ randomargs.c
@@ -6,6 +6,7 @@
 //
 // Run:
 //  $ ./randomargs one two three four five
+//
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -20,25 +21,26 @@
 int main(int argc, char *argv[]) {
     
     int n = 0;
-    int *rand_num_list = NULL;     //  list of random numbers from randomize();
+    // list of random numbers filled by randomize()
+    int *rand_num_list = NULL;                       
 
     // save arguments to dynamic array
     if (aalloc(argc, argv) != 0) exit(1);
 
 #ifdef DEBUG
-    // print starting data
+    // print starting data for debug
     printf("n = %d\n", n);
     for (n; n < argc; n++)
         printf("%s\n", aal_gp[n]); 
 #endif
-    n--;
 
-    // randomize args
+    // randomize args and print random order
+    // argc-1: no need to include the command name in the randomized list
     rand_num_list = randomize(argc-1);
-
     for (n = 0; n < argc-1; n++)
         printf("%s\n", argv[rand_num_list[n]+1]);
 
+    // clean up used memory no longer needed, but OS will do this anyway
     free(rand_num_list);
     free(aal_gp);
     return 0;
